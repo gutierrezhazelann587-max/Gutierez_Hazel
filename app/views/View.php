ee00982a0dd0858f6404f09b50c52dc505b3ddbf
gutierrezhazelann587-max <gutierrezhazelann587@gmail.com>
2025-09-25 09:20:59 +0800

    first commit

--- app/views/View.php
+++ app/views/View.php
@@ -3,33 +3,43 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>View</title>
+    <title>Welcome to Profile View</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+	<script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 </head>
 <body>
-    <h1>Welcome to View View</h1>
-    <a href="<?= '/create'; ?>">Create User</a>
-    <table border="1"   cellpadding="10" cellspacing="0">
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>Name</th>
-                <th>Email</th>
-                <th>Actions</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php foreach ($data as $user): ?>
-            <tr>
-                <td><?= ($user['id']); ?></td>
-                <td><?= ($user['name']); ?></td>
-                <td><?= ($user['email']); ?></td>
-                <td>
-                    <a href="<?= site_url('/edit/' . $user['id']); ?>">Edit</a>
-                    <a href="<?= site_url('/delete/' . $user['id']); ?>">Delete</a>
-                </td>
-            </tr>
-            <?php endforeach; ?>
-        </tbody>    
-    </table>
+	<div class="container mt-3 ">
+	<form action="<?=site_url('author');?>" method="get" class="col-sm-4 float-end d-flex">
+		<?php
+		$q = '';
+		if(isset($_GET['q'])) {
+			$q = $_GET['q'];
+		}
+		?>
+        <input class="form-control me-2" name="q" type="text" placeholder="Search" value="<?=html_escape($q);?>">
+        <button type="submit" class="btn btn-primary" type="button">Search</button>
+	</form>
+	<h2>Welcome to Profile View</h2>
+	<table class="table table-striped">
+		<thead>
+		<tr>
+			<th>Username</th>
+			<th>Email</th>
+			
+		</tr>
+		</thead>
+		<tbody>
+		<?php foreach(html_escape($all) as $users): ?>
+		<tr>
+		
+			<td><?=$users['username'];?></td>
+			<td><?=$users['email'];?></td>
+		</tr>
+		<?php endforeach; ?>
+		</tbody>
+	</table>
+	<?php
+	echo $page;?>
+	</div>
 </body>
 </html>
